f8ae080644d88f2e0893fc0a9653503d728991d1
BNAS24 <124627177+BNAS24@users.noreply.github.com>
2024-05-15 14:01:39 -0400

    ...

--- src/app/_components/buttons/HomepageButton1.tsx
+++ src/app/_components/buttons/HomepageButton1.tsx
@@ -4,27 +4,24 @@ import theme from "../../_styles/muiTheme";
 
 interface HomepageButtonProps {
   text: string;
-  variant: ButtonProps['variant']; // Define type based on ButtonProps
+  variant: ButtonProps["variant"]; // Define type based on ButtonProps
 }
 
-export const HomepageButton = ({
-  text,
-  variant,
-}: HomepageButtonProps) => {
-  
+export const HomepageButton = ({ text, variant }: HomepageButtonProps) => {
   return (
     <ThemeProvider theme={theme}>
       <Button
         variant={variant}
         disableElevation
         sx={{
-          mt: "16px",
-          height: "32px",
-          width: "64px",
-          borderRadius: "16px",
+          mt: "1rem",
+          height: "2rem",
+          width: "4rem",
+          borderRadius: "16rem",
           "&:hover": {
             color: variant ? theme.palette.primary.contrastText : null,
-            backgroundColor: variant === "outlined" ? theme.palette.primary.main : null,
+            backgroundColor:
+              variant === "outlined" ? theme.palette.primary.main : null,
           },
         }}
       >
