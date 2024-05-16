b4065e6e0194a1cb383fd4c5bd45bedbf78dfbb8
BNAS24 <124627177+BNAS24@users.noreply.github.com>
2024-05-16 16:57:14 -0400

    ...

--- src/app/lib/dbConnect.ts
+++ src/app/lib/dbConnect.ts
@@ -3,21 +3,10 @@ import mongoose from "mongoose";
 const connection: { isConnected?: number } = {};
 
 async function dbConnect() {
-  const uri = process.env.MONGODB_URI;
-
-  // Runtime check to ensure MONGODB_URI is defined
-  if (!uri) {
-    throw new Error(
-      "Please define the MONGODB_URI environment variable inside .env.local"
-    );
-  }
-
   if (connection.isConnected) {
     return;
   }
-
-  // Type assertion to ensure TypeScript treats uri as a string
-  const db = await mongoose.connect(uri as string);
+  const db = await mongoose.connect(process.env.MONGODB_URI!);
 
   connection.isConnected = db.connections[0].readyState;
   console.log("Connected to MongoDB");
